80c191dd0aa690746b4a5c7b0efc2d099a0a02d5
cal-stytch <>
2022-12-16 09:25:40 -0500

    Pull up

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,8 @@ import { StytchProvider } from '@stytch/react';
 import { StytchUIClient } from '@stytch/vanilla-js';
 
 // SET YOUR STYTCH PUBLIC TOKEN TO THIS VARIABLE. YOU CAN GET IT FROM THE STYTCH DASHBOARD AT https://stytch.com/dashboard/api-keys
-const STYTCH_PUBLIC_TOKEN = 'public-token-test-cc1b18f5-ae18-4b84-be92-0b8980347d79'
+const STYTCH_PUBLIC_TOKEN = 'public-token-test-5fb66ad9-f2cf-4e9f-9a35-218e6a4c227b'
+
 
 const stytch = new StytchUIClient(STYTCH_PUBLIC_TOKEN);
 
